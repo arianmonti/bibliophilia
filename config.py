7e71cfe0330176951e8f14c350359ec6b94bad15
arianmonti <a.m.ontazeri1385@gmail.com>
2020-08-13 22:08:15 +0430

    add environment

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key'
-    SQLALCHEMY_DATABASE_URI = 'mysql://username:password@host/bibliophilia'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['username@example.com']
-    POSTS_PER_PAGE = 1
+    ADMINS = os.environ.get('ADMINS') or ['email']
+    POSTS_PER_PAGE=2
+    LANGUAGES = ['en', 'fa']
