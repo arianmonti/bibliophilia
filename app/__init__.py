58ebf2986a98dfeb4dc6f43262b00ca6647cb3d8
arianmonti <a.m.ontazeri1385@gmail.com>
2020-08-31 21:40:08 +0430

    restore app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -85,7 +85,6 @@ def create_app(config_class=Config):
 
 @babel.localeselector
 def get_locale():
-    # return request.accept_languages.best_match(current_app.config['LANGUAGES'])
-    return 'fa'
+    return request.accept_languages.best_match(current_app.config['LANGUAGES'])
 
 from app import models
